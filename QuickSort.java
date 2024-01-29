6b5905ca911c45d25b96aee8e2b8f3cf82e61d7d
Vikas Kumar <115615649+Vk7017@users.noreply.github.com>
2024-01-29 14:39:51 +0530

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,59 @@
+package Assignment1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class QuickSort {
+	private static final QuickSort Quicksort = null;
+
+	static int partition(int array[], int low, int high) {
+	    int pivot = array[high];
+	    int i = (low - 1);
+
+	    for (int j = low; j < high; j++) {
+	      if (array[j] <= pivot) {
+
+	        i++;
+
+	        int temp = array[i];
+	        array[i] = array[j];
+	        array[j] = temp;
+	      }
+
+	    }
+
+	    int temp = array[i + 1];
+	    array[i + 1] = array[high];
+	    array[high] = temp;
+
+	    return (i + 1);
+	  }
+
+	  static void quickSort(int array[], int low, int high) {
+	    if (low < high) {
+
+	      int pi = partition(array, low, high);
+	      
+	      quickSort(array, low, pi - 1);
+
+	      quickSort(array, pi + 1, high);
+	    }
+	  }
+	
+	public static void main(String args[]) {
+		System.out.println("Enter your Array size:");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("Enter your Array Elemets:");
+		int []data=new int[n];
+		for(int i=0;i<n;i++){
+		    data[i]=sc.nextInt();
+		}
+	    int size = data.length;
+
+	    Quicksort.quickSort(data, 0, size - 1);
+
+	    System.out.println("Sorted Array in Ascending Order ");
+	    System.out.println(Arrays.toString(data));
+	  }
+	}
